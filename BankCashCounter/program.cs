078ba86846625fde5177cf73403be6be8bae85c9
17nidhi1997 <nidhinavya1997@gmail.com>
2020-04-11 15:02:10 +0530

    Main method

---
+++ BankCashCounter/program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CashCounter cash = new CashCounter();
+            cash.Queue();
+
+        }
+    }
+}
