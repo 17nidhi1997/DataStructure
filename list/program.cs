c45f4e101f25155a14fb246ba77347abe221a892
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-04-11 13:52:36 +0530

    Main Method

---
+++ list/program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace fileorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unordred aa = new unordred();
+            aa.unorderlist();
+            
+
+        }
+    }
+}
