1df6fb753d2e1c5dfe1e39a6160e1ca0b60cf7cd
17nidhi1997 <nidhinavya1997@gmail.com>
2020-04-11 15:24:54 +0530

    Main method

---
+++ calenderQueue/program.cs
@@ -0,0 +1,15 @@
+ using System;
+
+
+namespace stackpro
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 bb = new Class1();
+                    bb.aa();
+         }
+}
+}      
