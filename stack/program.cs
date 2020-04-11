897d2b75e3beb80f05b5d0c17b8f7ed0a85817b6
17nidhi1997 <nidhinavya1997@gmail.com>
2020-04-11 14:31:41 +0530

    Main method

---
+++ stack/program.cs
@@ -0,0 +1,18 @@
+ using System;
+
+
+namespace stackpro
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            Console.WriteLine("****************************Stack Operations*****************************");
+            BalanceParentheses check = new BalanceParentheses();
+            check.IsBalanceParentheses();
+     }
+   }
+}
