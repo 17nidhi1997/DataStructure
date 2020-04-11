fb0ae7b85cc6c209165f7539a8d40a6064a8f38a
17nidhi1997 <nidhinavya1997@gmail.com>
2020-04-11 14:12:32 +0530

    Main Method

---
+++ OList/program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************orderlist*****************");
+            OrderedList check = new OrderedList();
+            check.Ordered();
+        
+        }
+    }
+}
